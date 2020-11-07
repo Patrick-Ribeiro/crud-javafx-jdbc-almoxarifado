13c46ce96e94125d79b4244e9db5c3f120223bd1
Patrick-Ribeiro <patrickribeiro.rb@gmail.com>
2020-11-07 13:19:56 -0300

    Design main view e classe Loader

---
+++ src/ui/Loader.java
@@ -0,0 +1,38 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Loader extends Application {
+
+    private static Scene mainScene;
+
+    @Override
+    public void start(Stage primaryStage) {
+        String locationMainFXMl = "/ui/fxml/main.fxml";
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(locationMainFXMl));
+            Parent root = loader.load();
+            mainScene = new Scene(root, 1280, 720);
+
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.setTitle("Almoxarifado");
+            primaryStage.setScene(mainScene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            System.out.println("ERRO " + ex.getMessage()); //Implementar gerenciamento de logs
+        } catch (IllegalStateException ex) {
+            System.out.println("ERRO no arquivo FXML " + locationMainFXMl); //Implementar gerenciamento de logs
+        }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
+    }
+}
